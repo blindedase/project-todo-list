2ddd5a1303eed320795fab9bb11bdbec24008c3d
blindedase <blindedase@gmail.com>
2023-03-29 16:51:28 +0800

    completed todolist app but bad design

--- js/app.js
+++ js/app.js
@@ -1,55 +1,48 @@
-// Create a "close" button and append it to each list item
-var myNodelist = document.getElementsByTagName("LI");
-var i;
-for (i = 0; i < myNodelist.length; i++) {
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  myNodelist[i].appendChild(span);
-}
-
-// Click on a close button to hide the current list item
-var close = document.getElementsByClassName("close");
-var i;
-for (i = 0; i < close.length; i++) {
-  close[i].onclick = function() {
-    var div = this.parentElement;
-    div.style.display = "none";
+// GREET USER
+function insertName(){
+  const name = document.getElementById("user").value;
+  if (name != ``){
+    localStorage.setItem("userName", name);
+    window.location.href = "app/todo-list.html";
+  } else{
+    alert("Please enter your name.");
   }
 }
 
-// Add a "checked" symbol when clicking on a list item
-var list = document.querySelector('ul');
-list.addEventListener('click', function(ev) {
-  if (ev.target.tagName === 'LI') {
-    ev.target.classList.toggle('checked');
-  }
-}, false);
-
-// Create a new list item when clicking on the "Add" button
-function newElement() {
+// AD TO DO LIST
+function addList() {
   const li = document.createElement("li");
-  const inputValue = document.getElementById("userInput").value;
-  const t = document.createTextNode(inputValue);
-  li.appendChild(t);
-  if (inputValue === '') {
-    alert("You must write something!");
-  } else {
-    document.getElementById("unorderedList").appendChild(li);
-  }
-  document.getElementById("userInput").value = "";
+  const input = document.getElementById("add-task").value;
+  const a = document.createTextNode(input);
+  li.appendChild(a);
 
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
+  if(input == '') {
+      alert("Can't leave the input blank.");
+  } else {
+      document.getElementById("list").appendChild(li);
+  }
+  document.getElementById("add-task").value = "";
+
+  const span = document.createElement("SPAN");
+  const txt = document.createTextNode("\u00D7");
   span.className = "close";
   span.appendChild(txt);
   li.appendChild(span);
 
   for (i = 0; i < close.length; i++) {
     close[i].onclick = function() {
-      var div = this.parentElement;
+      const div = this.parentElement;
       div.style.display = "none";
     }
   }
+}
+
+// REMOVE BUTTON
+const close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+close[i].onclick = function() {
+  const div = this.parentElement;
+  div.style.display = "none";
+}
 }
